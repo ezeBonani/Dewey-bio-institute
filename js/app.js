bc52b4e7dfc62550f8c6afbcf873c313573d0a0a
Ezequiel Luis Bonani <eze.bonani@gmail.com>
2024-04-04 17:01:10 -0300

    app web version 1.0

---
+++ js/app.js
@@ -0,0 +1,56 @@
+const w = window,
+  d = document;
+
+const header = d.getElementById("header"),
+  logo = d.getElementById("logo"),
+  navbarToggler = d.getElementById("navbar-toggler"),
+  navLink = d.querySelectorAll(".nav-link"),
+  navbarNav = d.getElementById("navbarNav");
+
+/* ajuste de logo al scrollear */
+window.addEventListener("scroll", () => {
+  let scroll = d.documentElement.scrollTop;
+  if (w.matchMedia("(min-width: 1200px)").matches) {
+    if (scroll != 0) {
+      header.style.height = "40px";
+      logo.style.width = "7rem";
+    } else {
+      header.style.height = "100px";
+      setTimeout(() => {
+        logo.style.width =
+          "12rem"; /* agrego timer para mejorar la visual al cambiar de tamaño */
+      }, 100);
+    }
+  } else {
+    if (navbarToggler.ariaExpanded === "false") {
+      if (scroll != 0) {
+        header.style.height = "60px";
+        logo.style.width = "7rem";
+      } else {
+        setTimeout(() => {
+          logo.style.width =
+            "12rem"; /* agrego timer para mejorar la visual al cambiar de tamaño */
+        }, 150);
+        header.style.height = "100px";
+      }
+    }
+  }
+});
+
+/* ajuste de alto del header cuando despliego el menu al estar sticky */
+navbarToggler.addEventListener("click", () => {
+  if (navbarToggler.ariaExpanded === "true") {
+    header.style.height = "auto";
+  }
+});
+
+/* cierre del navbar cuando clikeo en link */
+navLink.forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log(navbarToggler.ariaExpanded);
+    console.log(navbarNav);
+    if (navbarToggler.ariaExpanded === "true") {
+      navbarNav.classList.remove("show");
+    }
+  });
+});
